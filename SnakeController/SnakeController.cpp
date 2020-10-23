c11dceafabfc071dbed4bfc1ec95e353187605b0
= <damian.lukasik@mail.com>
2020-10-23 17:14:54 +0200

    bez dynamic_cast

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -216,16 +216,16 @@ Controller::Segment Controller::getNewHead() const
 void Controller::receive(std::unique_ptr<Event> e)
 {
     try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
+        handleTimePassed((TimeoutInd const&)(*e));
     } catch (std::bad_cast&) {
         try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
+            handleDirectionChange((DirectionInd const&)(*e));
         } catch (std::bad_cast&) {
             try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
+                handleFoodPositionChange((FoodInd const&)(*e));
             } catch (std::bad_cast&) {
                 try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
+                    handleNewFood((FoodResp const&)(*e));
                 } catch (std::bad_cast&) {
                     throw UnexpectedEventException();
                 }
